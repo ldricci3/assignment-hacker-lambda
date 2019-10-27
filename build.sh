53e72864ef6defe47aaf3120e3354052690044f9
Joshua Redding <jmichael652@gmail.com>
2019-10-26 22:08:02 -0400

    Fixed build script

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
+#! /usr/bin/env bash
+
 if [[ -e ./build.sh ]]; then
 	rm -rf skill_env/
+	rm ./lambda.zip
 	pip3 install -r requirements.txt -t skill_env
 	cp lambda_function.py skill_env/
 	cp rds_config.py skill_env/
-	zip -r lambda.zip skill_env/*
+	cd skill_env
+	zip -r lambda.zip *
+	mv lambda.zip ../
 else
 	echo "Error: Enter directory with script before executing."
 fi
