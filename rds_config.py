de0d6ce0bf52a7ac8cbb0138c503981c73cb244b
Leonard Ricci <ldricci3@gmail.com>
2019-10-26 21:39:52 -0400

    added dependencies + some code for connecting to the database

---
+++ rds_config.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+#config file containing credentials for rds mysql instance
+db_username = "admin"
+db_password = "password"
+db_name = "alexadb"
+db_endpoint = "alexadb.cif8bzra3ohz.us-east-1.rds.amazonaws.com"
