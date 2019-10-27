f496aec06bb20a77fb8175b1b5c0556436182418
Leonard Ricci <ldricci3@gmail.com>
2019-10-26 23:42:14 -0400

    updated database name

--- rds_config.py
+++ rds_config.py
@@ -2,5 +2,5 @@
 #config file containing credentials for rds mysql instance
 db_username = "admin"
 db_password = "password"
-db_name = "alexadb"
+db_name = "alexa"
 db_endpoint = "alexadb.cif8bzra3ohz.us-east-1.rds.amazonaws.com"
